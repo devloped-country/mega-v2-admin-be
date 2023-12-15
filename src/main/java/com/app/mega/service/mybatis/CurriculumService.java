92dcdabef08bfad1c50a1a6cdcdc99bbde3eccee
soommin0019 <smlee0109@naver.com>
2023-12-15 21:55:34 +0900

    feature:커리큘럼 목록 저장 구현

--- src/main/java/com/app/mega/service/mybatis/CurriculumService.java
+++ src/main/java/com/app/mega/service/mybatis/CurriculumService.java
@@ -178,6 +178,18 @@ public class CurriculumService {
 
         curriculumRepository.delete(curriculum);
     }
+
+    @Transactional
+    public void editCurriculumList(CurriculumRequest request) {
+        int totalCount = curriculumRepository.prevCurriculumCount(request.getCourseId());   //커리큘럼 갯수
+        List<Curriculum> curriculums = curriculumRepository.findAllBycourse_id(request.getCourseId());  //커리큘럼 리스트 불러오기
+
+        for(int listNumber = 0; listNumber < totalCount; listNumber++) {
+            Curriculum curriculum = curriculums.get(listNumber);    //커리큘럼 n번째 커리큘럼 가져오기
+            curriculum.setListOrder(request.getOrderList());        //그 커리큘럼에 몇번째 넣을껀지 저장
+        }
+        curriculumRepository.saveAll(curriculums);
+    }
 }
 
         /*Map<String, Map<String, Object>> curriculumInfo = new HashMap<>();
